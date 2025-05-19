0487d084a2163949a16b3fb1448ae95db26cd51b
T0n0yan <dav.tonoyan.90@gmail.com>
2025-05-20 00:22:12 +0400

    FIX: resolve review

--- src/app/shared/ui/ProjectItem.tsx
+++ src/app/shared/ui/ProjectItem.tsx
@@ -4,7 +4,7 @@ import {StaticImageData} from "next/image";
 function ProjectItem({image, text}: { image: StaticImageData, text: string }) {
   return (<div className="w-full relative">
     <img src={image.src} alt={text} width="100%" height="100%" />
-    <span className="absolute top-0 left-0 bg-black text-white font-light text-[17px] !px-2 !py-1">{text}</span>
+    <span className="absolute top-0 left-0 bg-black text-white font-light text-[17px] px-2 py-1">{text}</span>
   </div>);
 }
 
